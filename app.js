d1571d8adaeb903a1a48b30a9bd618d27ad9f852
Syed-Nazeer9 <Syednazeer679@gmail.com>
2023-04-07 19:05:02 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,125 @@
+//Q #1
+
+/*let userInput= +prompt("Enter your First Number");
+let userInput2= +prompt("Enter your Second Number");
+
+if (userInput >= userInput2) {
+    console.log(userInput + " Is A Greater Number .")
+}else  {
+    console.log(userInput2 + " Is A Greater Number .")
+}*/
+
+//Q #2
+
+/*let userInput= +prompt("Enter a Number");
+ if (userInput < 0){
+    alert("Negative")
+ } else {
+    alert("Posative")
+ }*/
+
+ //Q #3
+ 
+ /*let userInput  = +prompt("Enter The First Number");
+ let userInput1 = +prompt("Enter The Second Number");
+ let userInput2 = +prompt("Enter The Third Number");
+ let userInput3 = +prompt("Enter The Fourth Number");
+ let userInput4 = +prompt("Enter The Five Number");
+
+ if(userInput >= userInput1  ) {
+    console.log(userInput +" Ia Greater Number")
+ }
+ else if (userInput1 >= userInput2){
+    console.log(userInput1 +" Is a Greater Number")
+ }
+ else if (userInput2 >= userInput3){
+    console.log(userInput2 +" Is a Greater Number")
+ }
+ else if (userInput3 >= userInput4){
+    console.log(user4 +" Is a Greater Number")
+ }
+ else {
+    console.log(userInput4 +" Is a Greater Number")
+ }*/
+
+ //Q #4
+
+ /*for (  i = 0; i <= 15; i++){
+    if (i % 2 === 0){
+        console.log(i +" is Even")
+    }
+    else{
+        console.log(i +" is odd")
+    }
+ }*/
+
+ //Q #5
+
+ /*let subject = 91;
+ let subject1 = 80;
+ let subject2 = 71;
+ let subject3 = 62;
+ let subject4 = 52;
+
+ let avrageMarks = (subject + subject1 + subject2 + subject3 + subject4) / 5;
+ console.log(avrageMarks);
+
+ if (avrageMarks >= 90){
+    console.log(avrageMarks + " Grade A")
+ }
+ else if  (avrageMarks >= 80){
+    console.log(avrageMarks + " Grade B")
+ }
+ else if  (avrageMarks >= 70){
+    console.log(avrageMarks + " Grade C")
+ }
+ else if  (avrageMarks >= 60){
+    console.log(avrageMarks + " Grade D")
+ }
+ else {
+    console.log(avrageMarks + " Grade F")
+ }*/
+
+ //Q #6
+
+ for (let i = 1; i <= 100 ; i++) {
+    if(i % 15 === 0){
+        document.write(i +"FizzBuzz <br>");
+    }else if(i % 5 === 0){
+        document.write(i +"Buzz <br>");
+    }else if(i % 3 === 0){
+        document.write(i +"Fizz <br>");
+    }else{
+        document.write(i + "<br>");
+    }
+ }
+
+
+
+ //Q #7
+
+ /*for (let i = 0 ; i <= 5; i++){ 
+    for (let j = 0 ; j<= i-1 ; j++){
+    document.write(" *");
+ }
+ document.write("<br>");
+ }*/
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
